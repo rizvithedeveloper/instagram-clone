bb4a29c7b4b5e6b7cbad8774ea9d975f415ecb0a
Muhammad Rizwan <rizwan.xammin@gmail.com>
2021-08-12 11:34:35 +0500

    Login Page Form completed

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -55,6 +55,36 @@ const Login = () => {
             Forget password?
           </ForgetPassword>
         </LoginPageBox>
+
+        <LoginPageBox>
+          <NotAccountText>Don't have an account?</NotAccountText>
+          <SignUpLink to="/accounts/emailsignup">Sign Up</SignUpLink>
+        </LoginPageBox>
+
+        <GetTheAppBox>
+          <GetTheAppText>Get the app.</GetTheAppText>
+          <DownloadAppRow>
+            <DownloadApp
+              href="https://apps.apple.com/app/instagram/id389801252?vt=lo"
+              target="_blank"
+            >
+              <DownloadAppImage
+                src="/assets/images/appstore.png"
+                alt="App Store"
+              />
+            </DownloadApp>
+
+            <DownloadApp
+              href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb&utm_campaign=loginPage&ig_mid=E3012C4D-DB60-4DAA-8C65-84CD703C6F79&utm_content=lo&utm_medium=badge"
+              target="_blank"
+            >
+              <DownloadAppImage
+                src="/assets/images/playstore.png"
+                alt="Play Store"
+              />
+            </DownloadApp>
+          </DownloadAppRow>
+        </GetTheAppBox>
       </LoginPageWrapper>
     </LoginPageContainer>
   );
@@ -75,7 +105,7 @@ const LoginPageWrapper = styled.div`
 
 const LoginPageBox = styled.div`
   background-color: #fff;
-  padding: 10px 40px;
+  padding: 20px 40px;
   width: 100%;
   border: 1px solid rgba(var(--b6a, 219, 219, 219), 1);
   border-radius: 1px;
@@ -115,9 +145,46 @@ const ForgetPassword = styled(Link)`
   font-size: 12px;
   font-weight: 500;
   margin-top: 15px;
-  margin-bottom: 10px;
   display: flex;
   justify-content: center;
+`;
+
+const NotAccountText = styled.span`
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const SignUpLink = styled(Link)`
+  color: #0095f6;
+  color: rgba(var(--d69, 0, 149, 246), 1);
+  font-size: 14px;
+  font-weight: 700;
+  margin-left: 5px;
+`;
+
+const GetTheAppBox = styled.div``;
+
+const GetTheAppText = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  margin: 20px 0px;
+`;
+
+const DownloadAppRow = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const DownloadApp = styled.a`
+  width: 135px;
+
+  &:first-child {
+    margin-right: 10px;
+  }
+`;
+
+const DownloadAppImage = styled.img`
+  width: 100%;
 `;
 
 export default Login;
